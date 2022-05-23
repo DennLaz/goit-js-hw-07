8b1304757515079b3fbca73e551a15eff86426e5
Denis Lazarenko <dentus25@gmail.com>
2022-05-23 21:31:29 +0300

    add Gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function addElementsGallery(arr) {
+  const listMarkup = arr
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+  galleryEl.insertAdjacentHTML("beforeend", listMarkup);
+}
+
+addElementsGallery(galleryItems);
+
+galleryEl.addEventListener("click", openModal);
+
+function openModal(eve) {
+  eve.preventDefault();
+  const currentImg = eve.target;
+
+  if (currentImg.nodeName !== "IMG") {
+    return;
+  }
+
+  const currentUrlEl = currentImg.dataset.source;
+
+  basicLightbox
+    .create(`<img width="1280" height="auto" src="${currentUrlEl}">`)
+    .show();
+}
