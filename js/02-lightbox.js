b65831de616a518ffebdd54b10d985294e9a136c
Denis Lazarenko <dentus25@gmail.com>
2022-05-23 21:31:53 +0300

    add_lightBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function addElementsGallery(arr) {
+  const listMarkup = arr
+    .map(({ description, original, preview }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+  galleryEl.insertAdjacentHTML("beforeend", listMarkup);
+}
+
+addElementsGallery(galleryItems);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
